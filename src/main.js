50f9d9135cfb71867c8a41dc8bd1079ad0f3013c
MB13WIN10 <mihailbab13@yandex.ru>
2022-05-07 14:16:09 +0300

    lil bugFix

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import App from './helper-app.vue'
 import store from './vuex/store'
-import './assets/styles/styles.scss'
 import router from './router/router'
 
 createApp(App).use(store).use(router).mount('#app')
